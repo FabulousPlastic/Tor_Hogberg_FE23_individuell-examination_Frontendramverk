ed30aaf0b291c980764c7f122861432e1b8cfb17
Skola <tor_skola@air-tor.home>
2024-06-27 00:11:29 +0200

    small layout change

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,24 +4,23 @@ import './AboutPage.css';
 const AboutPage = () => {
   return (
     <div className="about-container">
-      <svg width="0" height="0">
-        <defs>
-          <mask id="text-mask">
-            <rect width="100%" height="100%" fill="white" />
-            <text x="50%" y="50%" dy=".35em" textAnchor="middle" fontSize="120" fontWeight="bold" fill="black">TOR</text>
-          </mask>
-        </defs>
-      </svg>
       <div className="intro">
-        <span className="masked-text">I am</span>
-        <span className="large-text">TOR</span>
-        <span className="masked-text">since 1979</span>
+        <h1>          
+          <span className="large-text">T</span>
+          <span className="medium-text">O</span>
+          <span className="medium-text">R</span>          
+        </h1>
       </div>
-      <p className="description">exploring</p>
-      <p className="description">learning</p>
-      <p className="description">creating</p>
+      <div className="slogan-container">
+        <h2>
+          <span className="slogan">I am</span>
+          <span className="slogan">exploring</span>
+          <span className="slogan">learning</span>
+          <span className="slogan">creating</span>
+          <span className="slogan">since 1979</span>
+      </h2>
+      </div>
     </div>
   );
 };
-
 export default AboutPage;
